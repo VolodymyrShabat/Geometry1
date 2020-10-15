5a97301d3072bfd4a25339e2304ccbd971774390
VolodymyrShabat <volodymyrshabat@gmail.com>
2020-10-15 12:15:22 +0300

    last

---
+++ Geometry1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Geometry1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        public EllipseGeometry pe;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Введiть данi!!!!!!!");
+            }
+
+            else
+            {
+                Window1 w1 = new Window1();
+                w1.Show();
+                Window2 w2 = new Window2();
+                w2.Show();
+                Window3 w3 = new Window3();
+                w3.Show();
+                Window4 w4 = new Window4();
+                w4.Show();
+            }
+
+
+        }
+    }
+}
---
+++ Geometry1/Window4.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Geometry1
+{
+    /// <summary>
+    /// Interaction logic for Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+            Function();
+        }
+        public void Function()
+        {
+            Path p2 = new Path();
+            p2.StrokeThickness = 2;
+            p2.Stroke = System.Windows.Media.Brushes.Blue;
+            p2.Fill = System.Windows.Media.Brushes.Blue;
+
+            EllipseGeometry elf2 = new EllipseGeometry();
+            elf2.RadiusX = 1;
+            elf2.RadiusY = 1;
+            elf2.Center = new Point(int.Parse(((MainWindow)Application.Current.MainWindow).textBox1.Text.ToString()), 0);
+            p2.Data = elf2;
+            MainCanvas4.Children.Add(p2);
+            Path p1 = new Path();
+            p1.StrokeThickness = 2;
+            p1.Stroke = System.Windows.Media.Brushes.Blue;
+            p1.Fill = System.Windows.Media.Brushes.Blue;
+
+            EllipseGeometry elf1 = new EllipseGeometry();
+            elf1.RadiusX = 1;
+            elf1.RadiusY = 1;
+            elf1.Center = new Point(int.Parse(((MainWindow)Application.Current.MainWindow).textBox2.Text.ToString()), 0);
+            p1.Data = elf1;
+            MainCanvas4.Children.Add(p1);
+            double k = Math.Pow(Math.Abs(elf2.Center.X - elf1.Center.X), 2) / 4;
+            for (int x = -400; x <= 400; x++)
+            {
+                for (int y = -225; y <= 225; y++)
+                {
+                    double s = Math.Sqrt(Math.Pow(elf2.Center.X - x, 2) + Math.Pow(elf2.Center.Y - y, 2)) * Math.Sqrt(Math.Pow(elf1.Center.X - x, 2) + Math.Pow(elf1.Center.Y - y, 2));
+                    if (k * 2 > s && s > k)
+                    {
+
+                        Path p = new Path();
+                        p.StrokeThickness = 2;
+                        p.Stroke = System.Windows.Media.Brushes.Red;
+                        p.Fill = System.Windows.Media.Brushes.Red;
+
+                        EllipseGeometry elf = new EllipseGeometry();
+                        elf.RadiusX = 1;
+                        elf.RadiusY = 1;
+                        elf.Center = new Point(x, y);
+                        p.Data = elf;
+                        MainCanvas4.Children.Add(p);
+                    }
+                }
+            }
+            Line l1 = new Line();
+            l1.X1 = -400;
+            l1.Y1 = 0;
+            l1.X2 = 400;
+            l1.Y2 = 0;
+            l1.Stroke = Brushes.Black;
+            Line l2 = new Line();
+            l2.X1 = 0;
+            l2.Y1 = -225;
+            l2.X2 = 0;
+            l2.Y2 = 225;
+            l2.Stroke = Brushes.Black;
+            MainCanvas4.Children.Add(l1);
+            MainCanvas4.Children.Add(l2);
+        }
+    }
+}
